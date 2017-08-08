2e0780e8afdedee7c6f0dd20f07eecde890ec453
raiden <peckwood@users.noreply.github.com>
2017-08-08 18:44:53 +0800

    updated with baeldung tutorial, working

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-See: https://spring.io/blog/2013/05/11/content-negotiation-using-spring-mvc and https://spring.io/blog/2013/06/03/content-negotiation-using-views/
+Followed http://www.baeldung.com/spring-mvc-content-negotiation-json-xml and test with 
 
-### How this works?
+- http://localhost:8080/sia_p419_16.2.1_rest_content_negotiation/users.json (json)
+- http://localhost:8080/sia_p419_16.2.1_rest_content_negotiation/users.xml (XML)
+- http://localhost:8080/sia_p419_16.2.1_rest_content_negotiation/users (json(default))
+
+Note this doesn't work with Spring 4.0.7, only 4.3.4 or later.
+
+The content below was based on the author's book but I didn't fully understand so you can ignore.
+
+---
+
+How this works?
 
 1. user visit the URL http://localhost:8080/sia_p419_16.2.1_rest_content_negotiation/users
 2. the logical view name
@@ -27,7 +37,11 @@ Maybe it is like this, but this is how I thought:
          1. when a controller’s handler method finishes, a logical view name is usually returned. If the method doesn’t directly return a logical view name (if the method returns void, for example), the logical view name is derived from the request’s URL
          2. `rest.controller.UserController.users(long, int)` didn't directly return a logical view name
          3. The logic view name is `users`
-      3.  As `BeanNameViewResolver` resolves logical view name to bean.It resolves the View bean of name "users"
+      3. As `BeanNameViewResolver` resolves logical view name to bean.It resolves the View bean of name "users"
 
 
+---
 
+> https://spring.io/blog/2013/05/11/content-negotiation-using-spring-mvc
+>
+> https://spring.io/blog/2013/06/03/content-negotiation-using-views/
--- src/main/java/rest/config/WebConfig.java
+++ src/main/java/rest/config/WebConfig.java
@@ -9,10 +9,12 @@ import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.BeanNameViewResolver;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
 
 @Configuration
 @EnableWebMvc // enable Spring MVC
@@ -23,25 +25,30 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		configurer.ignoreAcceptHeader(true)
 		.favorPathExtension(true)
+		.favorParameter(false)
 		.useJaf(false)
-		.defaultContentType(MediaType.TEXT_HTML)
-		.mediaType("html", MediaType.TEXT_HTML)
-		.mediaType("xml", MediaType.APPLICATION_XML)
-		.mediaType("json", MediaType.APPLICATION_JSON);
+		.defaultContentType(MediaType.APPLICATION_JSON_UTF8);
 	}
+	
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.enableContentNegotiation(
+                new MappingJackson2XmlView(),
+                new MappingJackson2JsonView());
+    }
 
 	// assumes HTML
-	@Bean
+/*	@Bean
 	public ViewResolver cnViewResolver(ContentNegotiationManager cnm) {
 		ContentNegotiatingViewResolver cnvr = new ContentNegotiatingViewResolver();
 		cnvr.setContentNegotiationManager(cnm);
 		return cnvr;
-	}
+	}*/
 
-	@Bean
+/*	@Bean
 	public ViewResolver beanNameViewResolver() {
 		return new BeanNameViewResolver();
-	}
+	}*/
 
 	// view of name "users"
 	/*
@@ -49,8 +56,8 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	 * BeanNameViewResolver resolves the View declared in the users() method.
 	 * That’s because the bean name matches the logical view name.
 	 */
-	@Bean
+/*	@Bean
 	public View users() {
 		return new MappingJackson2JsonView();
-	}
+	}*/
 }
